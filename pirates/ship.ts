4f48462260f9c68017415a3e1ac9969e625c34ec
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-19 11:06:59 +0200

    w4d5 trial exam ship class init

---
+++ pirates/ship.ts
@@ -0,0 +1,33 @@
+'use strict';
+
+import Pirate from "./pirate";
+
+export default class Ship {
+    private pirates: Pirate[];
+    private hasCaptain: boolean;
+
+    constructor() {
+
+    }
+
+    addPirate() {
+
+    }
+
+    getPoorPirates(): Pirate[] {
+
+    }
+
+    getGold(): number {
+
+    }
+
+    lastDayOnTheShip() {
+
+    }
+
+    prepareForBattle() {
+        
+    }
+
+}
