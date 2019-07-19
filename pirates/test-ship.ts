18c8085bb63ec984d221636005183dc8dbbf8124
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-19 12:28:40 +0200

    w4d5 trial exam ship class addPirate method unit testing

--- pirates/test-ship.ts
+++ pirates/test-ship.ts
@@ -10,3 +10,18 @@ test('testing Ship class constructor', (t: any) => {
     t.equals(testShip.getCaptainExists(), false);
     t.end();
 });
+
+test('testing Ship class addPirate method', (t: any) => {
+    let testShip = new Ship();
+    testShip.addPirate(new Pirate('', 100, false, false));
+    t.equals(testShip.getPirates().length, 0);
+    testShip.addPirate(new Pirate('name', -1, false, false));
+    t.equals(testShip.getPirates().length, 0);
+    testShip.addPirate(new Pirate('Jack', 100, true, false));
+    t.equals(testShip.getPirates().length, 1);
+    t.equals(testShip.getCaptainExists(), true);
+    testShip.addPirate(new Pirate('BlackBeard', 100, true, false));
+    t.equals(testShip.getPirates().length, 1);
+    t.end();
+});
+
