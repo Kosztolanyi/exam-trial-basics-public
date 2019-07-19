4d00bf9fa9cd8e4a3d10c1611dbc890eed6cc9fe
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-19 10:05:37 +0200

    w4d5 trial exam main logic test

--- symmetric-matrix/test-isSymmetric.ts
+++ symmetric-matrix/test-isSymmetric.ts
@@ -10,3 +10,9 @@ test('testing invalid input error handling', (t: any) => {
     t.equals(isSymmetric([[2, 3], [4, 5, 6]]), undefined);
     t.end();
 });
+
+test('testing main logic', (t: any) => {
+    t.equals(isSymmetric([[1, 0, 1], [0, 2, 2], [1, 2, 5]]), true);
+    t.equals(isSymmetric([[7, 7, 7], [6, 5, 7], [1, 2, 1]]), false);
+    t.end();
+});
