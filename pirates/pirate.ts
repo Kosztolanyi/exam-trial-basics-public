f1f5f6040493c62072e5c85c4cb46418bd255b41
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-19 11:00:49 +0200

    w4d5 trial exam pirate class main logic

--- pirates/pirate.ts
+++ pirates/pirate.ts
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 export default class Pirate {
     private name: string;
@@ -7,19 +7,40 @@ export default class Pirate {
     private isCaptain: boolean;
     private hasWoodenLeg: boolean;
 
-    constructor() {
-
+    constructor(name: string, gold: number, captain: boolean, woodenleg: boolean) {
+        this.name = name;
+        this.goldAmount = gold;
+        this.healthPoints = 10;
+        this.isCaptain = captain;
+        this.hasWoodenLeg = woodenleg;
     }
 
     work() {
-
+        if (this.isCaptain) {
+            this.goldAmount += 10;
+            this.healthPoints -= 5;
+        }
+        else {
+            this.goldAmount++;
+            this.healthPoints--;
+        }
     }
 
     party() {
-
+        if (this.isCaptain) {
+            this.healthPoints += 10;
+        }
+        else {
+            this.healthPoints++;
+        }
     }
 
-    toString() {
-
+    toString(): string {
+        if (this.hasWoodenLeg) {
+            return `- Hello, I'm ${this.name}. I have a wooden leg and ${this.goldAmount} gold.`;
+        }
+        else {
+            return `- Hello, I'm ${this.name}. I still have my real legs and ${this.goldAmount} gold.`;
+        }
     }
 }
