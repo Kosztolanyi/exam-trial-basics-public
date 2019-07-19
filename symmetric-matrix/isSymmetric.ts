971a6b9a34842ad2909e13baed58586ae493b8a0
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-19 09:34:31 +0200

    w4d5 trial exam symmetric matrix input error handling

--- symmetric-matrix/isSymmetric.ts
+++ symmetric-matrix/isSymmetric.ts
@@ -1,5 +1,23 @@
 'use strict';
 
 const isSymmetric = (inputMatrix: number[][]): boolean => {
-    return;
+    if (inputMatrix == undefined) {
+        console.log('please provide input matrix')
+        return undefined;
+    }
+    else if (inputMatrix.length == 0) {
+        console.log('please provide non-empty matrix')
+        return undefined;
+    }
+    else {
+        for (let i: number = 0; i < inputMatrix.length; i++) {
+            if (inputMatrix.length != inputMatrix[i].length) {
+                console.log('please provide symmetric matrix');
+                return undefined;
+            }
+            else {
+
+            }
+        }
+    }
 }
