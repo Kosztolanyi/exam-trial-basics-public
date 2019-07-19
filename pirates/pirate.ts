fce9ab705978d191a2990cedb105026e0149eb03
SzabolcsSed <szabolcs.sed@gmail.com>
2019-07-19 10:53:45 +0200

    w4d5 trial exam pirate class init

---
+++ pirates/pirate.ts
@@ -0,0 +1,25 @@
+'use strict'
+
+export default class Pirate {
+    private name: string;
+    private goldAmount: number;
+    private healthPoints: number;
+    private isCaptain: boolean;
+    private hasWoodenLeg: boolean;
+
+    constructor() {
+
+    }
+
+    work() {
+
+    }
+
+    party() {
+
+    }
+
+    toString() {
+
+    }
+}
